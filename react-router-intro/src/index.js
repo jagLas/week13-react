40aa176a6c58cac53c0f08f88624d044a8016d6f
jagLas <jglasher@gmail.com>
2023-05-03 10:34:13 -0400

    more notes

--- react-router-intro/src/index.js
+++ react-router-intro/src/index.js
@@ -2,24 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter, Route, Link, NavLink, Switch } from 'react-router-dom';
 import Users from './Users';
 import Profile from './Profile';
 
 const Root = () => {
+  const handleClick = ()=> {
+    console.log('Thanks for clicking!')
+  }
+
   return (
     <BrowserRouter>
       <div>
-        <Route exact path="/">
-          <App />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-        <Route path="/users/:userId">
-          <Profile />
-        </Route>
+        <NavLink to="/" activeStyle={{ fontWeight: "bold" }}>App</NavLink>
+        <NavLink activeClassName='red' to="/users">Users</NavLink>
+        <NavLink activeClassName="green" to="/users/1">Andrew's Profile</NavLink>
+        <NavLink exact to="/" onClick={handleClick}>App with click handler</NavLink>
+
+        <Switch>
+          <Route exact path="/">
+            <App />
+          </Route>
+          <Route path="/users/:userId">
+            <Profile />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route>
+            <h1>404: Page not found</h1>
+          </Route>
+        </Switch>
+
       </div>
 
     </BrowserRouter>
