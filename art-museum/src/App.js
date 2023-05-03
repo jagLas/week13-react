058e0f4bfec748837a199206ae609292701dac76
jagLas <jglasher@gmail.com>
2023-05-03 14:00:38 -0400

    phase 3 complete

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,15 +1,25 @@
 import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
-import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import GalleryView from "./components/GalleryView";
 
 function App() {
   return (
     <>
       <GalleryNavigation galleries={harvardArt.records} />
-      <Route path ="/galleries/:galleryId">
-        <GalleryView galleries={harvardArt.records} />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <h2>Harvard Art Museum</h2>
+          <p>Look, but Don't Touch. Please select a gallery in the navigation bar</p>
+        </Route>
+        <Route path ="/galleries/:galleryId">
+          <GalleryView galleries={harvardArt.records} />
+        </Route>
+        <Route>
+          <h2>Page Not Found</h2>
+        </Route>
+      </Switch>
+
     </>
     
   );
