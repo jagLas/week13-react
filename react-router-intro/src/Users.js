a4290148af7f67b432780899dbeaeaeaa068266a
jagLas <jglasher@gmail.com>
2023-05-03 09:45:10 -0400

    practice finished

---
+++ react-router-intro/src/Users.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+function Users() {
+    return (
+        <h1>Hello from Users!</h1>
+    )
+}
+
+export default Users;
--- react-router-intro/src/index.js
+++ react-router-intro/src/index.js
@@ -2,10 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Users from './Users';
+import Profile from './Profile';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/users/:userId">
+          <Profile />
+        </Route>
+      </div>
+
+    </BrowserRouter>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
