6c3fb9fd714ae8e0980402865d018f93a736b4fa
jagLas <jglasher@gmail.com>
2023-05-03 09:26:11 -0400

    first commit

---
+++ intro-to-JSX/practice.js
@@ -0,0 +1,22 @@
+const hello = <h1>Hello World!</h1>
+
+const navBar = (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Profile</li>
+            <li>Settings</li>
+        </ul>
+    </nav>
+);
+
+const navList = (
+    <ul>
+      <li className="selected">
+        <a href="/pets">Pets</a>
+      </li>
+      <li>
+        <a href="/owners">Owners</a>
+      </li>
+    </ul>
+);
