7c2a5ef7586a44a14db3e384a3bd6260391dabfc
jagLas <jglasher@gmail.com>
2023-05-03 11:45:16 -0400

    practice complete

--- practice-for-week-14-react-router-navigation/src/components/Stocks/index.js
+++ practice-for-week-14-react-router-navigation/src/components/Stocks/index.js
@@ -1,7 +1,24 @@
+import { Redirect, useHistory } from "react-router-dom";
+
+let loggedIn = true;
+
 function Stocks() {
+  const handleClick = () => {
+    window.alert('Sending info to the DB!');
+    history.push('/')
+  }
+
+  let history = useHistory();
+  console.log(history)
+
+  if(loggedIn == false){
+    return <Redirect to="/not-logged-in"></Redirect>
+  }
+
   return (
     <div className='comp orange'>
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
