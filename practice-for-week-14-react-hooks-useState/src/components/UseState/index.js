014a11df0c05f2513a2290971ac9d2ceb8b94532
jagLas <jglasher@gmail.com>
2023-05-04 10:54:32 -0400

    practice complete

--- practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
+++ practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
@@ -1,16 +1,29 @@
 import './UseState.css';
+import { useState } from 'react';
 
 const UseState = () => {
+  const [theme, setTheme] = useState('light')
+  const [count, setCount] = useState(0)
+
+  function toggleTheme () {
+    if (theme === 'light') {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }
+
   return (
-    <div className="state">
+    <div className={`state ${theme}`}>
       <h1>UseState Component</h1>
-      <button>Dark</button>
-      <button>Light</button>
-      <h2>DISPLAY COUNT HERE</h2>
-      <button>
+      <button onClick={() => setTheme('dark')}>Dark</button>
+      <button onClick={() => setTheme('light')}>Light</button>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <h2>{count}</h2>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
         Increment
       </button>
-      <button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
         Decrement
       </button>
     </div>
