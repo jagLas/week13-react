6be0aedd2712a184425cb74d20f3a52bd9b38a7e
jagLas <jglasher@gmail.com>
2023-05-05 11:11:02 -0400

    practice complete

--- practice-for-week-14-react-explore-hooks-long-practice/src/components/ProductListItem/ProductListItem.js
+++ practice-for-week-14-react-explore-hooks-long-practice/src/components/ProductListItem/ProductListItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function ProductListItem({ product, isSelected, onClick }) {
+  // console.log('ProductListItem loaded')
   return (
     <div className={`product-list-item ${isSelected ? ' selected' : ''}`}>
       <img className="product-list-item-photo"
--- practice-for-week-14-react-explore-hooks-long-practice/src/components/ProductView/ProductView.js
+++ practice-for-week-14-react-explore-hooks-long-practice/src/components/ProductView/ProductView.js
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
 import './ProductView.css'
 
 function ProductView({ products }) {
 
-  // TODO: Replace with state variable
-  const [sideOpen, setSideOpen] = useState(true);
+  const [sideOpen, setSideOpen] = useState(() => {
+    const sidePanel = localStorage.getItem('sideOpen');
+    if (sidePanel == undefined) {
+      // console.log('no data stored')
+      return true;
+    } else {
+      return (sidePanel === 'true') ? true: false;
+    }
+  });
+
   const [selectedProduct, setSelectedProduct] = useState('');
+
+  useEffect(() => {
+    // console.log('checking if product is selected')
+    if (selectedProduct) {
+      // console.log('Setting sideOpen to true')
+      setSideOpen(true)
+    }
+  }, [selectedProduct])
+
+  useEffect(() => {
+    // console.log('checking if side is closed')
+    if (!sideOpen) {
+      // console.log('Setting product to ""')
+      setSelectedProduct('');
+    }
+    localStorage.setItem('sideOpen', sideOpen)
+  },[sideOpen])
+
+  // console.log('ProductView Loaded')
 
   return (
     <div className="product-view">
@@ -19,7 +46,10 @@ function ProductView({ products }) {
               isSelected={(selectedProduct.id === item.id) ? true: false}
               key={item.id}
               product={item}
-              onClick={() => setSelectedProduct(item)}
+              onClick={() => {
+                // console.log('SELECTED', item)
+                setSelectedProduct(item);
+              }}
             />
           )}
         </div>
@@ -28,7 +58,7 @@ function ProductView({ products }) {
         <div className="product-side-panel-toggle-wrapper">
           <div className="product-side-panel-toggle"
               onClick={() => {
-                console.log('TOGGLE SIDE PANEL');
+                // console.log('TOGGLE SIDE PANEL');
                 setSideOpen(prevSideOpen => !prevSideOpen)
               }}>
             {sideOpen ? '>' : '<'}
