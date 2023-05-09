3a262fd5d31eb05c6bd401f07e01d7c22bd79490
jagLas <jglasher@gmail.com>
2023-05-09 15:56:11 -0400

    practice complete

--- practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SetCoffeeBean.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SetCoffeeBean.js
@@ -1,9 +1,16 @@
+import { useCoffee } from "../context/CoffeeContext";
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+
+  const {coffeBean, setCoffeeBeanId} = useCoffee();
+
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
+        onChange={(e) => setCoffeeBeanId(e.target.value)}
+        value={coffeBean}
       >
         {coffeeBeans.map(bean => (
           <option
